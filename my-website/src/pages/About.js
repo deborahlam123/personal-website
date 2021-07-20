ef1321ed2ff4292714fd154b6224525ffc0bb244
“deborahlam123” <deborahlam123@gmail.com>
2021-07-20 20:57:53 +1000

    about page skeleton

--- my-website/src/pages/About.js
+++ my-website/src/pages/About.js
@@ -1,17 +1,53 @@
-// import './About.css';
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  aboutRoot: {
+    textAlign: "center",
+    margin: "5%",
+    
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+  },aboutText: {
+    margin: "10px",
+    padding: "8px 5%",
+    backgroundColor: "#ffcbf2",
+    borderRadius: "10px",
+  },
+  aboutImage: {
+    margin: "10px",
+    padding: "8px 5%",
+    backgroundColor: "#ffcbf2",
+    borderRadius: "10px",
+  },
+ 
+}));
 
 function About() {
-    return (
-      <div className="About">
-        <header className="About-header">
-          <p>
-            About here!
-          </p>
-          
-        </header>
+  const classes = useStyles();
+  return (
+    <div className={classes.aboutRoot}>
+      <h3>About me</h3>
+      <div className={classes.content}>
+        <Paper elevation={3} className={classes.aboutText}>
+          <div>
+          <p>Something about me</p>
+          </div>
+        </Paper>
+
+        <Paper elevation={3} className={classes.aboutImage}>
+          <h4>Pic of me?</h4>
+        </Paper>
       </div>
-    );
-  }
-  
-  export default About;
-  
+
+    </div>
+  );
+}
+
+export default About;
